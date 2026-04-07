d03b73f9254336156ea706862b25c11e9e65800b
Jatin Kaushik <kaushikjatin2608@gmail.com>
2026-04-07 23:20:41 +0530

    Fixed port for deployment

--- app.py
+++ app.py
@@ -204,4 +204,5 @@ def process():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
